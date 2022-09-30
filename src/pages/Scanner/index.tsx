8e7ac3512138fa37d2387c2f4aa6254f229e6255
Mayron <mayronfernandes01@gmail.com>
2022-09-30 09:19:22 -0300

    validate participant

--- src/pages/Scanner/index.tsx
+++ src/pages/Scanner/index.tsx
@@ -9,16 +9,22 @@ import Feedback from '../../UI/Feedback';
 import * as C from './style';
 import Scan from './components/Scan';
 import Result from './components/Result';
+import useParty from '../../Hooks/useParty';
 type props = {
   disableScan: () => void
 }
 const Scanner = ({ disableScan }: props) => {
+  const {partyID} = useParty();
   const [scanComplete, setScanComplete] = useState<boolean>(false);
   const [message, setMessage] = useState<Message>({message: '', stats: Stats.NONE});
 
   const validateCode = (code: string) => {
+    if(!partyID){
+      setScanComplete(false);
+      return;
+    }
     setMessage({message: 'Buscando participante', stats: Stats.LOADING});
-    validateParticipantCode(code, 'a')
+    validateParticipantCode(code, partyID)
     .then(() => {
       setMessage({message: 'Tudo certo por aqui!', stats: Stats.SUCCESS});
     })
